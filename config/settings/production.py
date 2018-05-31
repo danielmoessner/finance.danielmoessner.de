37f05c0524cae26e13704ac1ece5fb9eddef2ae5
moesibert <d-moessner@outlook.de>
2018-05-31 22:45:32 +0200

    added LOGGING to production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,3 +4,19 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = get_secret("ALLOWED_HOSTS")
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+    },
+}
