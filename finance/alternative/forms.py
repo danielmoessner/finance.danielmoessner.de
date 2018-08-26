fab65a2cdbb65d1fe973e752cb9ee55f1d03ba4e
moesibert <d-moessner@outlook.de>
2018-08-26 22:30:04 +0200

    added the necessary forms

---
+++ finance/alternative/forms.py
@@ -0,0 +1,134 @@
+from django.utils import timezone
+from django import forms
+
+from finance.alternative.models import Alternative
+from finance.alternative.models import Timespan
+from finance.alternative.models import Value
+from finance.alternative.models import Depot
+from finance.alternative.models import Flow
+from finance.core.utils import create_slug
+
+
+# depot
+class DepotForm(forms.ModelForm):
+    class Meta:
+        model = Depot
+        fields = (
+            "name",
+        )
+
+    def __init__(self, user, *args, **kwargs):
+        super(DepotForm, self).__init__(*args, **kwargs)
+        self.instance.user = user
+
+
+class DepotSelectForm(forms.Form):
+    depot = forms.ModelChoiceField(widget=forms.Select, queryset=None)
+
+    class Meta:
+        fields = (
+            "depot",
+        )
+
+    def __init__(self, user, *args, **kwargs):
+        super(DepotSelectForm, self).__init__(*args, **kwargs)
+        self.fields["depot"].queryset = user.banking_depots.all()
+
+
+class DepotActiveForm(forms.ModelForm):
+    class Meta:
+        model = Depot
+        fields = ("is_active",)
+
+    def __init__(self, user, *args, **kwargs):
+        super(DepotActiveForm, self).__init__(*args, **kwargs)
+        user.banking_depots.update(is_active=False)
+
+
+# alternative
+class AlternativeForm(forms.ModelForm):
+    class Meta:
+        model = Alternative
+        fields = (
+            "name",
+        )
+
+    def __init__(self, depot, *args, **kwargs):
+        super(AlternativeForm, self).__init__(*args, **kwargs)
+        self.instance.depot = depot
+
+    def save(self, commit=True):
+        self.instance.slug = create_slug(self.instance, self.instance.name)
+        return super(AlternativeForm, self).save(commit=commit)
+    
+
+# value
+class ValueForm(forms.ModelForm):
+    date = forms.DateTimeField(widget=forms.DateTimeInput(attrs={"type": "datetime-local"},
+                                                          format="%Y-%m-%dT%H:%M"),
+                               input_formats=["%Y-%m-%dT%H:%M"], label="Date")
+
+    class Meta:
+        model = Value
+        fields = (
+            "alternative",
+            "date",
+            "value",
+        )
+
+    def __init__(self, depot, *args, **kwargs):
+        super(ValueForm, self).__init__(*args, **kwargs)
+        self.fields["alternative"].queryset = depot.alternatives.all()
+        self.fields["date"].initial = timezone.now()
+
+
+# change
+class FlowForm(forms.ModelForm):
+    date = forms.DateTimeField(widget=forms.DateTimeInput(attrs={"type": "datetime-local"},
+                                                          format="%Y-%m-%dT%H:%M"),
+                               input_formats=["%Y-%m-%dT%H:%M"], label="Date")
+
+    class Meta:
+        model = Flow
+        fields = (
+            "alternative",
+            "date",
+            "flow",
+        )
+
+    def __init__(self, depot, *args, **kwargs):
+        super(FlowForm, self).__init__(*args, **kwargs)
+        self.fields["alternative"].queryset = depot.alternatives.all()
+        self.fields["date"].initial = timezone.now()
+
+
+# timespan
+class TimespanForm(forms.ModelForm):
+    start_date = forms.DateTimeField(widget=forms.DateTimeInput(
+        attrs={"type": "datetime-local"}, format="%Y-%m-%dT%H:%M"),
+        input_formats=["%Y-%m-%dT%H:%M"], label="Date")
+    end_date = forms.DateTimeField(widget=forms.DateTimeInput(
+        attrs={"type": "datetime-local"}, format="%Y-%m-%dT%H:%M"),
+        input_formats=["%Y-%m-%dT%H:%M"], label="Date")
+
+    class Meta:
+        model = Timespan
+        fields = (
+            "name",
+            "start_date",
+            "end_date"
+        )
+
+    def __init__(self, depot, *args, **kwargs):
+        super(TimespanForm, self).__init__(*args, **kwargs)
+        self.instance.depot = depot
+
+
+class TimespanActiveForm(forms.ModelForm):
+    class Meta:
+        model = Timespan
+        fields = ("is_active",)
+
+    def __init__(self, depot, *args, **kwargs):
+        super(TimespanActiveForm, self).__init__(*args, **kwargs)
+        depot.timespans.update(is_active=False)
