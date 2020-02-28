038ffe7d411b1924447f2005cf9bae0d42e8fd9e
alessandrokonrad <akonrad6@gmail.com>
2020-02-28 16:51:29 +0100

    added frontend

--- finance/alternative/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AlternativeConfig(AppConfig):
-    name = 'finance.alternative'
--- finance/users/tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-from django.core.urlresolvers import reverse
-from django.test import Client
-
-# Create your tests here.
