bb959057765fb456d362c08adbf6136c12266ceb
dmoetech <github@dmoe.tech>
2020-03-26 12:41:32 +0100

    error views improved

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,6 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path
 
-
 urlpatterns = [
     path('', lambda request: redirect('users/redirect/', permanent=False)),
     path('users/', include('apps.users.urls')),
@@ -29,10 +28,23 @@ urlpatterns = [
     path('alternative/', include('apps.alternative.urls')),
 ]
 
-
 if settings.DEBUG:
+    # static and media
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    # admin
     urlpatterns += [path('admin/', admin.site.urls), ]
+
+    # debug toolbar
     import debug_toolbar
+
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls)), ]
+
+    # show the error views
+    from django.views.generic import TemplateView
+
+    urlpatterns += path('400/', TemplateView.as_view(template_name='400.html')),
+    urlpatterns += path('403/', TemplateView.as_view(template_name='403.html')),
+    urlpatterns += path('404/', TemplateView.as_view(template_name='404.html')),
+    urlpatterns += path('500/', TemplateView.as_view(template_name='500.html')),
