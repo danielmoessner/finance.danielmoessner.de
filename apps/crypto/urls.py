923bfdb4972da18440e795cbf02c4b7f340ad2d2
dmoetech <github@dmoe.tech>
2020-03-29 20:57:17 +0200

    removed old charts

--- apps/crypto/urls.py
+++ apps/crypto/urls.py
@@ -3,63 +3,45 @@ from django.urls import path
 from apps.crypto.views import views
 from apps.crypto.views import form_views
 
-
 app_name = "crypto"
 
-
 urlpatterns = [
-    # API DATA
-    # api-data/index/
-    path("api/index/", views.IndexData.as_view(), name="api_data_index"),
-    # api-data/account/bittrex/
-    path("api/account/<slug>/",  views.AccountData.as_view(), name="api_data_account"),
-    # api-data/assets/
-    path("api/assets/", views.AssetsData.as_view(), name="api_data_assets"),
-    # api-data/asset/bitcoin/
-    path("api/asset/<slug>/", views.AssetData.as_view(), name="api_data_asset"),
-
-    # FUNCTIONS
-    # update-movies/
+    # functions
     path("update-movies/", views.update_movies, name="update_movies"),
-    # reset-movies/
     path("reset-movies/", views.reset_movies, name="reset_movies"),
 
     # depot
-    path("depot/add/", form_views.AddDepotView.as_view(), name="add_depot"),
-    path("depot/delete/", form_views.DeleteDepotView.as_view(), name="delete_depot"),
-    path("depot/<pk>/edit/", form_views.EditDepotView.as_view(), name="edit_depot"),
-    path("depot/<pk>/set-active/", form_views.SetActiveDepotView.as_view(), name="set_depot"),
+    path("depots/add/", form_views.AddDepotView.as_view(), name="add_depot"),
+    path("depots/delete/", form_views.DeleteDepotView.as_view(), name="delete_depot"),
+    path("depots/<int:pk>/edit/", form_views.EditDepotView.as_view(), name="edit_depot"),
+    path("depots/<int:pk>/set-active/", form_views.SetActiveDepotView.as_view(), name="set_depot"),
 
     # account
-    path("account/add/", form_views.AddAccountView.as_view(), name="add_account"),
-    path("account/delete/", form_views.DeleteAccountView.as_view(), name="delete_account"),
-    path("account/<slug>/edit/", form_views.EditAccountView.as_view(), name="edit_account"),
+    path("accounts/add/", form_views.AddAccountView.as_view(), name="add_account"),
+    path("accounts/delete/", form_views.DeleteAccountView.as_view(), name="delete_account"),
+    path("accounts/<int:pk>/edit/", form_views.EditAccountView.as_view(), name="edit_account"),
 
     # asset
-    path("asset/add/", form_views.AddAssetView.as_view(), name="add_asset"),
-    path("asset/remove/", form_views.RemoveAssetView.as_view(), name="remove_asset"),
+    path("assets/add/", form_views.AddAssetView.as_view(), name="add_asset"),
+    path("assets/remove/", form_views.RemoveAssetView.as_view(), name="remove_asset"),
 
     # trade
-    path("trade/add/", form_views.AddTradeView.as_view(), name="add_trade"),
-    path("account/<slug>/trade/add/", form_views.AddTradeAccountView.as_view(), name="add_trade"),
-    path("trade/<pk>/edit/", form_views.EditTradeView.as_view(), name="edit_trade"),
-    path("trade/<pk>/delete/", form_views.DeleteTradeView.as_view(), name="delete_trade"),
+    path("trades/add/", form_views.AddTradeView.as_view(), name="add_trade"),
+    path("trades/<int:pk>/edit/", form_views.EditTradeView.as_view(), name="edit_trade"),
+    path("trades/<int:pk>/delete/", form_views.DeleteTradeView.as_view(), name="delete_trade"),
 
     # transaction
-    path("transaction/add/", form_views.AddTransactionView.as_view(), name="add_transaction"),
-    path("transaction/<pk>/edit/", form_views.EditTransactionView.as_view(), name="edit_transaction"),
-    path("transaction/<pk>/delete/", form_views.DeleteTransactionView.as_view(), name="delete_transaction"),
+    path("transactions/add/", form_views.AddTransactionView.as_view(), name="add_transaction"),
+    path("transactions/<int:pk>/edit/", form_views.EditTransactionView.as_view(), name="edit_transaction"),
+    path("transactions/<int:pk>/delete/", form_views.DeleteTransactionView.as_view(), name="delete_transaction"),
 
     # timespan
-    path("timespan/add/", form_views.AddTimespanView.as_view(), name="add_timespan"),
-    path("timespan/<pk>/delete/", form_views.DeleteTimespanView.as_view(), name="delete_timespan"),
-    path("timespan/<pk>/set/", form_views.SetActiveTimespanView.as_view(), name="set_timespan"),
+    path("timespans/add/", form_views.AddTimespanView.as_view(), name="add_timespan"),
+    path("timespans/<int:pk>/delete/", form_views.DeleteTimespanView.as_view(), name="delete_timespan"),
+    path("timespans/<int:pk>/set/", form_views.SetActiveTimespanView.as_view(), name="set_timespan"),
 
-    # PAGES
-    # index
+    # views
     path("depots/<int:pk>/", views.IndexView.as_view(), name="index"),
-    # account/bittrex/
-    path("account/<slug>/", views.AccountView.as_view(), name="account"),
-    # asset/bitcoin/
-    path("asset/<slug>/", views.AssetView.as_view(), name="asset"),
+    path("accounts/<int:pk>/", views.AccountView.as_view(), name="account"),
+    path("assets/<int:pk>/", views.AssetView.as_view(), name="asset"),
 ]
--- apps/crypto/views/views.py
+++ apps/crypto/views/views.py
@@ -1,6 +1,4 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.exceptions import ObjectDoesNotExist
-from django.contrib import messages
 from django.views import generic
 from django.conf import settings
 from django.http import HttpResponseRedirect
@@ -8,15 +6,7 @@ from django.urls import reverse_lazy
 
 from apps.crypto.models import Asset, Account, Depot
 from apps.crypto.tasks import update_movies_task
-from apps.core.utils import create_paginator
 from apps.core.views import TabContextMixin
-
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from background_task.models import Task
-from rest_framework.views import APIView
-import json
 
 
 class IndexView(LoginRequiredMixin, TabContextMixin, generic.DetailView):
@@ -75,7 +65,6 @@ class AssetView(LoginRequiredMixin, TabContextMixin, generic.DetailView):
         return context
 
 
-# functions
 def update_movies(request, *args, **kwargs):
     if settings.DEBUG:
         depot = request.user.crypto_depots.get(is_active=True)
@@ -90,155 +79,3 @@ def reset_movies(request, *args, **kwargs):
     depot = request.user.crypto_depots.get(is_active=True)
     depot.reset_movies(delete=True)
     return HttpResponseRedirect(reverse_lazy("crypto:index"))
-
-
-# api
-def json_data(pi, g=True, p=True, v=True, cr=True, twr=True, cs=True):
-    labels = pi["d"]
-
-    datasets = list()
-    if g:
-        data_profit = dict()
-        data_profit["label"] = "Profit"
-        data_profit["data"] = pi["g"]
-        data_profit["yAxisID"] = "value"
-        datasets.append(data_profit)
-    if p:
-        data_price = dict()
-        data_price["label"] = "Price"
-        data_price["data"] = pi["p"]
-        data_price["yAxisID"] = "value"
-        datasets.append(data_price)
-    if v:
-        data_value = dict()
-        data_value["label"] = "Value"
-        data_value["data"] = pi["v"]
-        data_value["yAxisID"] = "value"
-        datasets.append(data_value)
-    if cr:
-        data_cr = dict()
-        data_cr["label"] = "Current return"
-        data_cr["data"] = map(lambda x: x * 100, pi["cr"])
-        data_cr["yAxisID"] = "yield"
-        datasets.append(data_cr)
-    if twr:
-        data_twr = dict()
-        data_twr["label"] = "True time weighted return"
-        data_twr["data"] = map(lambda x: x * 100, pi["twr"])
-        data_twr["yAxisID"] = "yield"
-        datasets.append(data_twr)
-    if cs:
-        data_cs = dict()
-        data_cs["label"] = "Invested Capital"
-        data_cs["data"] = pi["cs"]
-        data_cs["yAxisID"] = "value"
-        datasets.append(data_cs)
-    data = dict()
-    data["labels"] = labels
-    data["datasets"] = datasets
-    return Response(data)
-
-
-class IndexData(APIView):
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
-    permission_classes = (IsAuthenticated,)
-
-    def get(self, request, format=None):
-        user = request.user
-        try:
-            depot = user.crypto_depots.get(is_active=True)
-        except Depot.DoesNotExist:
-            depot = user.crypto_depots.first()
-        timespan = depot.timespans.get(is_active=True)
-        pi = depot.get_movie().get_data(timespan)
-        return json_data(pi, p=False)
-
-
-class AccountData(APIView):
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
-    permission_classes = (IsAuthenticated,)
-
-    def get(self, request, slug, format=None):
-        user = request.user
-        try:
-            depot = user.crypto_depots.get(is_active=True)
-        except Depot.DoesNotExist:
-            depot = user.crypto_depots.first()
-        account = depot.accounts.get(slug=slug)
-        assets = depot.assets.exclude(symbol=user.get_currency_display())
-        movies = depot.movies.filter(account=account, asset__in=assets).select_related("asset")
-        datasets = list()
-        labels = list()
-        data = list()
-        for movie in movies:
-            try:
-                value = movie.pictures.latest("d").v
-            except ObjectDoesNotExist:
-                continue
-            if round(value, 2) != 0.00:
-                labels.append(str(movie.asset))
-                data.append(value)
-        data_and_labels = list(sorted(zip(data, labels)))
-        labels = [l for d, l in data_and_labels]
-        data = [abs(d) for d, l in data_and_labels]
-        datasets_data = dict()
-        datasets_data["data"] = data
-        datasets.append(datasets_data)
-
-        data = dict()
-        data["datasets"] = datasets
-        data["labels"] = labels
-        return Response(data)
-
-
-class AssetsData(APIView):
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
-    permission_classes = (IsAuthenticated,)
-
-    def get(self, request, format=None):
-        user = request.user
-        try:
-            depot = user.crypto_depots.get(is_active=True)
-        except Depot.DoesNotExist:
-            depot = user.crypto_depots.first()
-        assets = depot.assets.exclude(symbol=user.get_currency_display())
-        movies = depot.movies.filter(account=None, asset__in=assets).select_related("asset")
-        datasets = list()
-        labels = list()
-        data = list()
-        for movie in movies:
-            try:
-                picture = movie.pictures.latest("d")
-            except ObjectDoesNotExist:
-                continue
-            value = picture.v
-            if value and round(value, 2) != 0.00:
-                labels.append(str(movie.asset))
-                data.append(value)
-        data_and_labels = list(sorted(zip(data, labels)))
-        labels = [l for d, l in data_and_labels]
-        data = [abs(d) for d, l in data_and_labels]
-        datasets_data = dict()
-        datasets_data["data"] = data
-        datasets.append(datasets_data)
-
-        data = dict()
-        data["datasets"] = datasets
-        data["labels"] = labels
-        return Response(data)
-
-
-class AssetData(APIView):
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
-    permission_classes = (IsAuthenticated,)
-
-    def get(self, request, slug, format=None):
-        user = request.user
-        try:
-            depot = user.crypto_depots.get(is_active=True)
-        except Depot.DoesNotExist:
-            depot = user.crypto_depots.first()
-        asset = Asset.objects.get(slug=slug)
-        timespan = depot.timespans.get(is_active=True)
-        pi = asset.get_movie(depot).get_data(timespan)
-        return json_data(pi, twr=False)
