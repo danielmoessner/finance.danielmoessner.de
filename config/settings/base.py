b4413e1a5f147ae2f287de8ab94bf0e295819abb
danielmoessner <github@moesibert.de>
2021-05-01 17:03:13 +0200

    settings changed

--- config/settings/base.py
+++ config/settings/base.py
@@ -141,11 +141,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/app'),
 ]
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'tmp/static')
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
