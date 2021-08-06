b0053a5ddc5f55934326ec669bb7e42d2199fc5d
danielmoessner <github@moesibert.de>
2021-08-06 19:44:47 +0200

    switch to bootstrap 5

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -1,33 +1,46 @@
-import $ from 'jquery';
-window.jQuery = $;
-window.$ = $;
-import 'popper.js';
 import 'bootstrap';
 
+// wrapper function that calls the submitted function as soon as the document is loaded
+function ready(f) {
+    if (document.readyState !== "loading") {
+        f()
+    } else {
+        document.addEventListener('DOMContentLoaded', f)
+    }
+}
+
 // put all modals to the end of the body so that they work fine on mobile
-$(window).on('load', function () {
-    $(".modal").appendTo("body")
+ready(function () {
+    document.querySelectorAll('.modal').forEach(modal => document.body.appendChild(modal));
 });
 
 // bootstrap modal form fetch data (the form) if it is shown
-$(window).on('load', function () {
-    $(".modal").on('show.bs.modal', function (event) {
-        const url = $(event.target).find("form").attr('action');
-        const body = $(event.target).find(".modal-body");
-        if (typeof url !== "undefined" && typeof body !== "undefined") fetchAndInsert(url, body);
-    })
+ready(function () {
+    document.querySelectorAll('.modal').forEach(
+        modal => {
+            modal.addEventListener('shown.bs.modal', function (event) {
+                const url = event.target.querySelector("form").action;
+                const body = event.target.querySelector(".modal-body");
+                if (typeof url !== "undefined" && typeof body !== "undefined") fetchAndInsert(url, body);
+            })
+        }
+    )
 });
 
 // bootstrap form modal post the data on submit with an fetch request
-$(window).on('load', function () {
-    $(".modal form").on("submit", function (event) {
-        event.preventDefault();
-        const url = $(event.target).attr('action');
-        const data = $(event.target).serialize();
-        const errorElement = $(event.target).find(".modal-body");
-        if (typeof url !== "undefined" && typeof data !== "undefined" && typeof errorElement !== "undefined")
-            postData(url, data, errorElement);
-    });
+ready(function () {
+    document.querySelectorAll(".modal form").forEach(
+        form => {
+            form.addEventListener("submit", function (event) {
+                event.preventDefault();
+                const url = event.target.action;
+                const data = new FormData(form);
+                const errorElement = event.target.querySelector(".modal-body");
+                if (typeof url !== "undefined" && typeof data !== "undefined" && typeof errorElement !== "undefined")
+                    postData(url, data, errorElement);
+            })
+        }
+    )
 });
 
 // post data to an url but insert the html of the response back into an element if an error occurs
@@ -35,18 +48,15 @@ function postData(postUrl, formData, elementToInsertErrorInto) {
     fetch(postUrl, {
         method: "post",
         body: formData,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
     })
         .then((response) => {
             return response.json()
         })
         .then((data) => {
-            data.valid ? window.location.reload() : $(elementToInsertErrorInto).html(data.html);
+            data.valid ? window.location.reload() : elementToInsertErrorInto.innerHTML = data.html;
         })
         .catch((error) => {
-            $(elementToInsertErrorInto).html("Error");
+            elementToInsertErrorInto.innerHTML = "Error";
         });
 }
 
@@ -57,6 +67,6 @@ function fetchAndInsert(urlToFetch, elementToInsertTo) {
             return response.text();
         })
         .then((data) => {
-            elementToInsertTo.html(data);
+            elementToInsertTo.innerHTML = data;
         });
 }
