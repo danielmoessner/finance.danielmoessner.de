2215c66218a64dfde4e8323c7737fa30aa6ec9c1
Daniel Mössner <github@moesibert.de>
2023-11-06 17:41:15 +0100

    static files clean up

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,4 @@ git pull
 tmp/venv/bin/pip install -r requirements.txt
 tmp/venv/bin/python manage.py migrate
 ./permissions.sh
-tmp/venv/bin/python manage.py collectstatic  --no-input
 systemctl restart apache2
