06447e238ad0238a26e38b563b0fca4fb672a308
Daniel Mössner <github@moesibert.de>
2023-11-06 17:41:46 +0100

    add back

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ git pull
 tmp/venv/bin/pip install -r requirements.txt
 tmp/venv/bin/python manage.py migrate
 ./permissions.sh
+tmp/venv/bin/python manage.py collectstatic  --no-input
 systemctl restart apache2
