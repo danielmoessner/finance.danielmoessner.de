77eb501cee7319d4297add0abb7762dcb0dff781
Daniel Mössner <github@moesibert.de>
2024-02-28 22:35:08 +0100

    buckets

--- apps/overview/forms.py
+++ apps/overview/forms.py
@@ -6,7 +6,10 @@ from .models import Bucket
 class BucketForm(forms.ModelForm):
     class Meta:
         model = Bucket
-        fields = ("name",)
+        fields = (
+            "name",
+            "wanted_percentage",
+        )
 
     def __init__(self, user, *args, **kwargs):
         super(BucketForm, self).__init__(*args, **kwargs)
--- apps/overview/models.py
+++ apps/overview/models.py
@@ -1,5 +1,6 @@
 from typing import TYPE_CHECKING
 
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from apps.users.models import StandardUser
@@ -18,6 +19,9 @@ class Bucket(models.Model):
     user = models.ForeignKey(
         StandardUser, on_delete=models.CASCADE, related_name="buckets"
     )
+    wanted_percentage = models.IntegerField(
+        default=0, validators=[MinValueValidator(1), MaxValueValidator(100)]
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -26,6 +30,10 @@ class Bucket(models.Model):
         crypto_items: QuerySet["Asset"]
         alternative_items: QuerySet["Alternative"]
         stocks_items: QuerySet["Stock"]
+
+    @property
+    def wanted_percentage_str(self) -> str:
+        return "{:,.2f} %".format(self.wanted_percentage)
 
     def __str__(self):
         return self.name
@@ -39,20 +47,32 @@ class Bucket(models.Model):
         return i5
 
     def __get_amount(self) -> float:
-        items = self.get_items()
-        if not items:
-            return 0
-        amount = sum([item.get_bucket_value() for item in items])
-        return amount
+        if not hasattr(self, "_amount"):
+            items = self.get_items()
+            if not items:
+                return 0
+            self._amount = sum([item.get_bucket_value() for item in items])
+        return self._amount
 
     def get_amount(self) -> str:
         amount = self.__get_amount()
         return "{:,.2f} €".format(amount)
 
+    def __get_total(self) -> float:
+        if not hasattr(self, "_total"):
+            buckets = list(Bucket.objects.filter(user=self.user))
+            self._total = sum([bucket.__get_amount() for bucket in buckets])
+        return self._total
+
     def get_percentage(self) -> str:
-        buckets = list(Bucket.objects.filter(user=self.user))
+        total = self.__get_total()
         amount = self.__get_amount()
-        total = sum([bucket.__get_amount() for bucket in buckets])
         if total == 0:
             return "0.00 %"
         return "{:,.2f} %".format(amount / total * 100)
+
+    def get_diff(self) -> str:
+        wanted = self.wanted_percentage * self.__get_total() / 100
+        amount = self.__get_amount()
+        diff = wanted - amount
+        return "{:,.2f} €".format(diff)
