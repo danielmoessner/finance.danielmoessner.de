67020a5dc2c159d7aeec2d82c7d027695a00ff6b
Daniel Mössner <github@moesibert.de>
2024-07-03 17:45:51 +0200

    djlint simple

--- precommit.sh
+++ precommit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf "djlint:\n"
-pdm run djlint . ./pyproject.toml --max-line-length=120 --profile=jinja --reformat
+pdm run djlint . --max-line-length=120 --profile=jinja --reformat --include="*.j2"
 printf "\nisort:\n" && pdm run isort . --profile black
 printf "\n\n"
 printf "black:\n" && pdm run black .
