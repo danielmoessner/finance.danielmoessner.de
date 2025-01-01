066583ede807052b36f97b45d6c78a851c33d893
danielmoessner <github@danielmoessner.de>
2025-01-01 22:31:17 +0100

    add loader on submit button

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -57,8 +57,15 @@ ready(function () {
                 const url = event.target.action;
                 const data = new FormData(form);
                 const errorElement = event.target.querySelector(".modal-body");
+                const btn = form.querySelector("button[type='submit']");
+                const prevBtn = btn.innerHTML;
+                btn.innerHTML = `${btn.innerHTML} <svg class="animate-spin" style="width: 16px; margin-bottom: 1px; height: 16px;" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>`;
+                btn.disabled = true;
                 if (typeof url !== "undefined" && typeof data !== "undefined" && typeof errorElement !== "undefined")
-                    postData(url, data, errorElement);
+                    postData(url, data, errorElement).finally(() => {
+                        btn.innerHTML = prevBtn;
+                        btn.disabled = false;
+                    });
             })
         }
     )
@@ -66,7 +73,7 @@ ready(function () {
 
 // post data to an url but insert the html of the response back into an element if an error occurs
 function postData(postUrl, formData, elementToInsertErrorInto) {
-    fetch(postUrl, {
+    return fetch(postUrl, {
         method: "post",
         body: formData,
     })
@@ -84,7 +91,7 @@ function postData(postUrl, formData, elementToInsertErrorInto) {
 // fetches data from an url and inserts it into an element
 function fetchAndInsert(urlToFetch, elementToInsertTo) {
     elementToInsertTo.innerHTML = "Loading...";
-    fetch(urlToFetch)
+    return fetch(urlToFetch)
         .then((response) => {
             return response.text();
         })
