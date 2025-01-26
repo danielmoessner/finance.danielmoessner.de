dd7892ac9afbd1903cf7555649cfeb5f2797d17e
danielmoessner <github@danielmoessner.de>
2025-01-26 22:42:30 +0100

    use pip and install from pyproject

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 cd /home/finance.danielmoessner.de/ || exit
 git reset --hard HEAD
 git pull
-tmp/venv/bin/pip install -r requirements.txt
+tmp/venv/bin/pip install .
 tmp/venv/bin/python manage.py migrate
 ./permissions.sh
 tmp/venv/bin/python manage.py collectstatic  --no-input
