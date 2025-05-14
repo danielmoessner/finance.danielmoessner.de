62ab95bf4f97f9f00f06aba2f026de6b88dbf613
danielmoessner <github@danielmoessner.de>
2025-05-14 19:50:32 +0200

    improve logging

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,11 +7,18 @@ ALLOWED_HOSTS = get_secret("ALLOWED_HOSTS")
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
+    "formatters": {
+        "custom": {
+            "format": "%(asctime)s: %(message)s",
+            "datefmt": "%d.%m.%Y %H:%M",
+        },
+    },
     "handlers": {
         "info": {
             "level": "INFO",
             "class": "logging.FileHandler",
             "filename": os.path.join(BASE_DIR, "tmp/django.log"),
+            "formatter": "custom",
         },
     },
     "loggers": {
